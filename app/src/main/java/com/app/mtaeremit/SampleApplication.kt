0a1fbf3a4819258493b8e102a8b378e8e030370b
Raju Gotame <munnadroid@Rajus-MacBook-Pro.local>
2021-09-29 17:07:58 +0545

    Remove Multidex

--- app/src/main/java/com/app/mtaeremit/SampleApplication.kt
+++ app/src/main/java/com/app/mtaeremit/SampleApplication.kt
@@ -1,13 +1,8 @@
 package com.app.mtaeremit
 
-import android.content.Context
-import androidx.multidex.MultiDex
 import com.google.android.play.core.splitcompat.SplitCompatApplication
 
-class SampleApplication:SplitCompatApplication() {
+class SampleApplication : SplitCompatApplication() {
 
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        MultiDex.install(this)
-    }
+
 }
